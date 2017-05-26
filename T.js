a41b949fe5f0b947a967de5401c8a4140474b8ac
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-05-26 13:04:20 -0500

    Update and rename T.java to T.js

---
+++ T.js
@@ -0,0 +1,22 @@
+class Ball {
+int color = black;
+int float = ;
+int x = ;
+  int y = ;
+}
+public class Box1 {
+int color = White;
+int length = 5;
+int width = 10;
+  int x = ;
+  int y = ;
+  
+}
+
+if () {
+}
+
+public static void main(String [ ] args) {
+
+
+}
