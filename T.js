da492ce20ca2536bdc2d8420e66f5a5ec6fbea30
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-05-26 13:11:54 -0500

    Update T.js

--- T.js
+++ T.js
@@ -1,22 +1,2 @@
-class Ball {
-int color = black;
-int float = ;
-int x = ;
-  int y = ;
-}
-public class Box1 {
-int color = White;
-int length = 5;
-int width = 10;
-  int x = ;
-  int y = ;
-  
-}
+var ball = new.object();
 
-if () {
-}
-
-public static void main(String [ ] args) {
-
-
-}
