e2daff50ad10ce7fc968aecab8b32e47f33eb349
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-05-30 10:55:12 -0500

    Update T.js

--- T.js
+++ T.js
@@ -1,10 +1,8 @@
-var canvas = document.getElementById("Background");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
+var c=document.getElementById("Background");
+var ctx=c.getContext("2d");
+ctx.fillStyle="#FF0000";
 ctx.fillRect(20,20,150,100);
 
-var canvas = document.getElementById("Background");
-var ctx = canvas.getContext("2d");
 ctx.moveTo(20, 0);
 ctx.lineTo(180, 100);
 ctx.stroke();
