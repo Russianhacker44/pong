789d849cbf4196b714e5fba7f80a814894d153e3
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-05-30 10:51:00 -0500

    Update T.js

--- T.js
+++ T.js
@@ -1,7 +1,7 @@
 var canvas = document.getElementById("Background");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
-ctx.fillRect(20, 0, 180, 100);
+ctx.fillRect(20,20,150,100);
 /// 20, 0 to 20, 100 
 ctx.moveTo(20, 0);
 ctx.lineTo(180, 100);
