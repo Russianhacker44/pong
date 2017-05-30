c1d0bda24727b3c6b77a88000bcdf2491da54f10
Russianhacker44 <Browns4lyfe01@gmail.com>
2017-05-30 10:47:12 -0500

    Update T.js

--- T.js
+++ T.js
@@ -1,10 +1,10 @@
 var canvas = document.getElementById("Background");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
-ctx.fillRect(20, 0, 20, 100);
+ctx.fillRect(20, 0, 180, 100);
 /// 20, 0 to 20, 100 
 ctx.moveTo(20, 0);
-ctx.lineTo(20, 100);
+ctx.lineTo(180, 100);
 ctx.stroke();
 
 document.body.onkeyup = function(e){
